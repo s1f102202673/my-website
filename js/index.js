f254d45964d38bb81543db95110f430950ac5676
s1f10220260 <s1f102202606@iniad.org>
2022-07-15 15:13:45 +0900

    スライドショーの追加

--- js/index.js
+++ js/index.js
@@ -13,3 +13,26 @@ window.addEventListener('load',()=>{
 
     });
 });
+
+const pics_src1 = ["うなぎ.jpg","うどん.jpg","ハンバーガー.jpg"];
+const pics_src2 = ["水族館.jpg","SKY CIRCUS.jpg","nanja town.jpg"];
+
+let num = -1;
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("picture_food").src = pics_src1[num];
+  document.getElementById("picture_atraction").src = pics_src2[num];
+}
+ 
+setInterval(slideshow_timer, 2000);
+
+
+
+
+
+ 
